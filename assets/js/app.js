c02e9fb40d7503fa704b82a07cc8163ae9ae0587
michaelresser <michaelresser@gmail.com>
2020-03-19 22:51:51 -0400

    animAtion success! animated scroll behavior of splash and projects page.

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+import SplashController from './lib/splash__controller.js';
+// app.js test
+console.log('test')
+  
+// initialize splash control animation function
+let splashController = new SplashController();
+
+
+
+
